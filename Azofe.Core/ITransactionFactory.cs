da781dbc1217660a161f1a9441b57502824f13ad
Félix Severo <felixsevero@gmail.com>
2024-09-16 09:41:35 -0300

    Add async methods to transactions

--- Azofe.Core/ITransactionFactory.cs
+++ Azofe.Core/ITransactionFactory.cs
@@ -1,13 +1,17 @@
 namespace Azofe.Core;
 
-public interface ITransaction: IDisposable {
+public interface ITransaction: IAsyncDisposable, IDisposable {
 
 	void Complete();
+
+	Task CompleteAsync();
 
 }
 
 public interface ITransactionFactory {
 
 	ITransaction CreateTransaction();
+
+	Task<ITransaction> CreateTransactionAsync();
 
 }
